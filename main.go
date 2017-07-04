31214eec206e01ac5854330b5496aa8befad9b00
Dmitry <irvis@imega.ru>
2017-07-04 09:57:05 +0300

    split files offer and stock

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
 	"os"
+	"bufio"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/imega-teleport/xml2db/mysql"
@@ -11,7 +14,56 @@ import (
 )
 
 func main() {
-	xmlFile, err := os.Open("import.xml")
+	var fileImport bool
+	user, pass, host := os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST")
+	dbname := flag.String("db", "", "Database name")
+	f := flag.String("file", "", "File to parse")
+	flag.Parse()
+	dsn := fmt.Sprintf("mysql://%s:%s@tcp(%s)/%s", user, pass, host, *dbname)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		os.Exit(1)
+	}
+	err = db.Ping()
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		os.Exit(1)
+	}
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			fmt.Printf("error: %v", err)
+			return
+		}
+		fmt.Println("Closed db connection")
+	}()
+
+	xmlFile1, err := os.Open(*f)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		os.Exit(1)
+	}
+	defer xmlFile1.Close()
+
+	scanner := bufio.NewScanner(xmlFile1)
+	for scanner.Scan() {
+		t := scanner.Text()
+		if strings.Contains(t, "Классификатор") {
+			fileImport = true
+			break
+		}
+		if strings.Contains(t, "ПакетПредложений") {
+			fileImport = false
+			break
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("reading file: %v", err)
+		os.Exit(1)
+	}
+
+	xmlFile, err := os.Open(*f)
 	if err != nil {
 		fmt.Printf("error: %v", err)
 		os.Exit(1)
@@ -31,56 +83,13 @@ func main() {
 		os.Exit(1)
 	}
 
-    user, pass, host, dbname := "root", "", "10.0.3.94:3306", "teleport"
-	dsn := fmt.Sprintf("mysql://%s:%s@tcp(%s)/%s", user, pass, host, dbname)
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		os.Exit(1)
-	}
-    err = db.Ping()
-    if err != nil {
-        fmt.Printf("error: %v", err)
-        os.Exit(1)
-    }
-    defer func() {
-        err := db.Close()
-        if err != nil {
-            fmt.Printf("error: %v", err)
-            return
-        }
-        fmt.Println("Closed db connection")
-    }()
-
 	storage := mysql.NewStorage(db)
 	parser := v204.NewParser204(storage)
-	err = parser.Parse(data)
-    if err != nil {
-        fmt.Printf("error: %v", err)
-        os.Exit(1)
-    }
-
-	xmlFile, err = os.Open("offers.xml")
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		os.Exit(1)
+	if fileImport == true {
+		err = parser.Parse(data)
+	} else {
+		err = parser.ParseBundling(data)
 	}
-	defer xmlFile.Close()
-
-	stat, err = xmlFile.Stat()
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		os.Exit(1)
-	}
-
-	data = make([]byte, stat.Size())
-	_, err = xmlFile.Read(data)
-	if err != nil {
-		fmt.Printf("error: %v", err)
-		os.Exit(1)
-	}
-
-	err = parser.ParseBundling(data)
 	if err != nil {
 		fmt.Printf("error: %v", err)
 		os.Exit(1)
