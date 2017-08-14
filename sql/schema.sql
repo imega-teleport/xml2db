2742b17efe241bbadc50a172050311fe65e25f73
Dmitry <irvis@imega.ru>
2017-08-14 09:11:04 +0300

    len id 36 to 80

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,15 +4,15 @@ USE teleport;
 
 DROP TABLE IF EXISTS groups;
 CREATE TABLE groups (
-  id        VARCHAR(36) NOT NULL,
-  parent_id VARCHAR(36),
+  id        VARCHAR(80) NOT NULL,
+  parent_id VARCHAR(80),
   name      VARCHAR(3000),
   KEY record(id, parent_id)
 );
 
 DROP TABLE IF EXISTS properties;
 CREATE TABLE properties (
-  id   VARCHAR(36) NOT NULL,
+  id   VARCHAR(80) NOT NULL,
   name VARCHAR(3000),
   type VARCHAR(3000),
   KEY record(id)
@@ -20,7 +20,7 @@ CREATE TABLE properties (
 
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
-  id          VARCHAR(36) NOT NULL,
+  id          VARCHAR(80) NOT NULL,
   name        VARCHAR(3000),
   description TEXT,
   barcode     VARCHAR(250),
@@ -33,23 +33,23 @@ CREATE TABLE products (
 
 DROP TABLE IF EXISTS products_properties;
 CREATE TABLE products_properties (
-  parent_id VARCHAR(36) NOT NULL,
-  id        VARCHAR(36) NOT NULL,
+  parent_id VARCHAR(80) NOT NULL,
+  id        VARCHAR(80) NOT NULL,
   value     VARCHAR(250),
   KEY record(parent_id, id)
 );
 
 DROP TABLE IF EXISTS products_taxes;
 CREATE TABLE products_taxes (
-  parent_id VARCHAR(36) NOT NULL,
-  name      VARCHAR(36),
+  parent_id VARCHAR(80) NOT NULL,
+  name      VARCHAR(80),
   rate      VARCHAR(20),
   KEY record(parent_id)
 );
 
 DROP TABLE IF EXISTS products_requisites;
 CREATE TABLE products_requisites (
-  parent_id VARCHAR(36) NOT NULL,
+  parent_id VARCHAR(80) NOT NULL,
   name      VARCHAR(250),
   value     VARCHAR(250),
   KEY record(parent_id)
@@ -57,7 +57,7 @@ CREATE TABLE products_requisites (
 
 DROP TABLE IF EXISTS products_excises;
 CREATE TABLE products_excises (
-  parent_id VARCHAR(36) NOT NULL,
+  parent_id VARCHAR(80) NOT NULL,
   name      VARCHAR(250),
   sum       FLOAT,
   currency  CHAR(3),
@@ -66,22 +66,22 @@ CREATE TABLE products_excises (
 
 DROP TABLE IF EXISTS products_images;
 CREATE TABLE products_images (
-  parent_id VARCHAR(36) NOT NULL,
+  parent_id VARCHAR(80) NOT NULL,
   url       VARCHAR(250),
   KEY record(parent_id)
 );
 
 DROP TABLE IF EXISTS products_groups;
 CREATE TABLE products_groups (
-  parent_id VARCHAR(36) NOT NULL,
-  id        VARCHAR(36) NOT NULL,
+  parent_id VARCHAR(80) NOT NULL,
+  id        VARCHAR(80) NOT NULL,
   KEY record(parent_id)
 );
 
 DROP TABLE IF EXISTS products_contractor;
 CREATE TABLE products_contractor (
-  parent_id VARCHAR(36) NOT NULL,
-  id        VARCHAR(36) NOT NULL,
+  parent_id VARCHAR(80) NOT NULL,
+  id        VARCHAR(80) NOT NULL,
   name      VARCHAR(250),
   title     VARCHAR(3000),
   full_name VARCHAR(3000),
@@ -90,26 +90,26 @@ CREATE TABLE products_contractor (
 
 DROP TABLE IF EXISTS products_component;
 CREATE TABLE products_component (
-  parent_id     VARCHAR(36) NOT NULL,
-  catalog_id    VARCHAR(36) NOT NULL,
-  classifier_id VARCHAR(36) NOT NULL,
+  parent_id     VARCHAR(80) NOT NULL,
+  catalog_id    VARCHAR(80) NOT NULL,
+  classifier_id VARCHAR(80) NOT NULL,
   quantity      SMALLINT,
   KEY record(parent_id)
 );
 
 DROP TABLE IF EXISTS bundling;
 CREATE TABLE bundling (
-  id            VARCHAR(36) NOT NULL,
+  id            VARCHAR(80) NOT NULL,
   name          VARCHAR(3000),
-  catalog_id    VARCHAR(36),
-  classifier_id VARCHAR(36),
+  catalog_id    VARCHAR(80),
+  classifier_id VARCHAR(80),
   KEY record(id)
 );
 
 DROP TABLE IF EXISTS bundling_offers;
 CREATE TABLE bundling_offers (
-  parent_id        VARCHAR(36) NOT NULL,
-  id               VARCHAR(36) NOT NULL,
+  parent_id        VARCHAR(80) NOT NULL,
+  id               VARCHAR(80) NOT NULL,
   name             VARCHAR(3000),
   base_unit        VARCHAR(200),
   base_unit_name   VARCHAR(200),
@@ -121,15 +121,15 @@ CREATE TABLE bundling_offers (
 
 DROP TABLE IF EXISTS bundling_prices_types;
 CREATE TABLE bundling_prices_types (
-  parent_id VARCHAR(36) NOT NULL,
+  parent_id VARCHAR(80) NOT NULL,
   KEY record(parent_id)
 );
 
 DROP TABLE IF EXISTS bundling_offers_prices;
 CREATE TABLE bundling_offers_prices (
-  parent_id     VARCHAR(36) NOT NULL,
+  parent_id     VARCHAR(80) NOT NULL,
   display       VARCHAR(200),
-  price_type_id VARCHAR(36) NOT NULL,
+  price_type_id VARCHAR(80) NOT NULL,
   unit_price    VARCHAR(50),
   currency      CHAR(3),
   unit          VARCHAR(50),
